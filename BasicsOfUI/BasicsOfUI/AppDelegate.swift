6f127c7fef1f03b7be50ea150c8cbada44799c0c
Rajan Patel <rajan12233445@gmail.com>
2023-02-20 10:23:33 +0530

    Basics Of UI

--- BasicsOfUI/BasicsOfUI/AppDelegate.swift
+++ BasicsOfUI/BasicsOfUI/AppDelegate.swift
@@ -26,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+<<<<<<< Updated upstream
         
         
         // Called when the user discards a scene session.
@@ -37,6 +38,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         
         
+=======
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+>>>>>>> Stashed changes
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
